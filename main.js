445875b77cf7a2a3334e2aec0468551c594505fb
esbabaes <esbabaes37@gmail.com>
2020-12-03 14:41:33 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,76 +1,62 @@
 var liste_al,liste_ekle,liste_topla=[];
-var sayi=0,topla=0,sira=0,kura;
+var sayi=0,topla=0,kura;
+
 
 var liste_goster=document.querySelector("#liste_goster");
 var kura_sonuc=document.querySelector("#kura_sonuc");
 
 function liste_cek(){
-    liste_al=document.querySelector("#kura_liste").value;
-    liste_ekle=liste_al.split(",");
-    if(liste_al==""){
-        alert("listeye isim yaz...");
-    }
+   liste_al=document.querySelector("#kura_liste").value;
+   liste_ekle=liste_al.split(",");
     
+   if(liste_al==""){
+       alert("listeye eleman ekle...");
+   }else{
+       liste_goster.innerHTML="";
+       for(sayi=0;sayi<liste_ekle.length;sayi++){
+           liste_topla[topla]=liste_ekle[sayi];
+           topla++;
+       }
+       for(topla=0;topla < liste_topla.length;topla++){
+           liste_goster.innerHTML+= "<span>"+liste_topla[topla]+"</span>";
+       }
+   } 
     
-    if(liste_al !=""){
-    for(sayi=0; sayi<liste_ekle.length; sayi++){
-        liste_topla[topla]=liste_ekle[sayi];
-        topla++;
-    }
-     liste_goster.innerHTML="";   
-    for(sira=0;sira<topla;sira++){
-        liste_goster.innerHTML+="<span>"+liste_topla[sira]+"</span>";
-    }    
-    
-}
- liste_al=document.querySelector("#kura_liste").value="";   
-    
+    liste_al=document.querySelector("#kura_liste").value="";
 }
 
 function liste_cek_temizle(){
-    liste_al=document.querySelector("#kura_liste").value=""; 
+    liste_al=document.querySelector("#kura_liste").value="";
 }
 
 function kura_cek(){
-    
-    
-    
     if(liste_goster.innerHTML=="liste göster"){
         alert("listeye eleman gonder...");
-    }else {
-        
+    }else{
         kura_sonuc.innerHTML="";
-        for(sayi=0;sayi<3;sayi++){
-        kura=Math.floor(Math.random()*liste_topla.length);
-          kura_sonuc.innerHTML+="<span>"+liste_topla[kura]+"</span>";  
-        }
-        
+       for(sayi=0;sayi<3;sayi++){
+           kura=Math.floor(Math.random()*liste_topla.length);
+           kura_sonuc.innerHTML+= "<span>" +liste_topla[kura]+"</span>";
+           
+       }
         
     }
-    
-    
-    
 }
 
 function kura_cek_temizle(){
     liste_goster.innerHTML="liste göster";
     liste_topla=[];
     topla=0;
-    
 }
 
+
 function kura_yazdir(){
-    var sonuc= kura_sonuc.innerText;
+    var sonuc =kura_sonuc.innerText;
     alert(sonuc);
 }
 
-function kura_temizle(){
+function kura_yazdir_temizle(){
     kura_sonuc.innerHTML="kura sonuc";
 }
-
-
-
-
-
 
 
